927285cdbfd025fc402d9264f549db63287df97f
Daniel Boghossian <dboghossian@esri.com>
2023-07-26 15:11:01 -0700

    Started work on hand animations

---
+++ samples_project/Assets/SampleViewer/Samples/VRSample/Scripts/AnimateHandOnInput.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AnimateHandOnInput : MonoBehaviour
+{
+    [SerializeField] private InputActionProperty pinchAnimationAction;
+    [SerializeField] private Animator handAnimator;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float triggerValue = pinchAnimationAction.action.ReadValue<float>();
+        handAnimator.SetFloat("Trigger", triggerValue);
+    }
+}
